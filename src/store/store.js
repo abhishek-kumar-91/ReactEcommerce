26ef9df517b5e40cce6d8b87a13cb89a90040a68
Abhishek Kumar <abhishekkumar958830@gmail.com>
2023-10-04 22:03:24 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import {configureStore} from '@reduxjs/toolkit';
 import cartReducer from '../reducer/cartSlice';
 import productReducer from '../reducer/productSlice';
 
-
+//newstore
 const store = configureStore({
     reducer:{
         cart: cartReducer,
